1754cf9832d42f9c0fc2be2322bea0c89cc5ee95
Adam Henry <henryada@oregonstate.edu>
2024-07-04 15:32:15 -0700

    change api for incidents to NIFC, add perimeters from NIFC, switch to geoJSON

--- public/assets/javascript/main.js
+++ public/assets/javascript/main.js
@@ -1,100 +1,108 @@
+let all_markers = []
+let wildfire_markers = []
+let current_markers = []
+
 let display_headers = () => {
-	let main = document.getElementById("main")
+	let main = document.getElementsByTagName("header")[0]
 	let subheader = document.createElement("h2")
-	subheader.innerHTML = "Incident List"
+	subheader.innerHTML = "Incident Map (WIP)"
 	main.appendChild(subheader)
 }
 
-let display_resources_table = (url) => {
-	let json = fetch(url)
+let display_resources_table = (json) => {
+	let table = document.createElement("table")
+	let table_body = document.createElement("tbody")
 
-	json.then((response) => {
-		return response.json()
-	}).then((json) => {
-		let table = document.createElement("table")
-		let table_body = document.createElement("tbody")
+	let header = table.createTHead()
+	let header_row = header.insertRow(-1)
+	let name = header_row.insertCell(0)
+	let description = header_row.insertCell(1)
+	let type = header_row.insertCell(2)
+	let status = header_row.insertCell(3)
+	let date = header_row.insertCell(4)
 
-		let header = table.createTHead()
-		let header_row = header.insertRow(-1)
-		let name = header_row.insertCell(0)
-		let description = header_row.insertCell(1)
-		let type = header_row.insertCell(2)
-		let status = header_row.insertCell(3)
-		let date = header_row.insertCell(4)
-
-		name.innerHTML = "Name"
-		description.innerHTML = "Description"
-		type.innerHTML = "Web Comment"
-		status.innerHTML = "Status"
-		date.innerHTML = "Date"
-
-		positions = []
-		json[0].data.forEach((resource) => {
-			if (resource.latitude && resource.longitude) {
-				positions.push({ lat: parseFloat(resource.latitude), lng: -parseFloat(resource.longitude) })
-			}
-
-			let row = table_body.insertRow(-1)
-			let name = row.insertCell(0)
-			let description = row.insertCell(1)
-			let type = row.insertCell(2)
-			let status = row.insertCell(3)
-			let date = row.insertCell(4)
-
-			name.innerHTML = resource.name
-			description.innerHTML = resource.type
-			type.innerHTML = resource.webComment
-			status.innerHTML = resource.fire_status
-			date.innerHTML = resource.date
-		})
-
-		let main = document.getElementById("main")
-		table.appendChild(table_body)
-		main.appendChild(table)
-		initMap(json)
-	}).catch((error) => {
-		console.log(error)
-	})
-}
-
-// google maps api/library
-(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
-	key: "AIzaSyDBXUMhIDQ9J904-bATL_euXi5AlXHc6yg",
-	v: "weekly",
-	// Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
-	// Add other bootstrap parameters as needed, using camel case.
-});
-
-// Initialize and add the map
-let map;
-
-async function initMap(json) {
-	// Request needed libraries.
-	//@ts-ignore
-	const position = { lat: -25.344, lng: 131.031 };
-	const { Map } = await google.maps.importLibrary("maps");
-	const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
-	// The map, centered at Uluru
-	map = new Map(document.getElementById("map"), {
-		zoom: 4,
-		center: { lat: 44.065, lng: -120.607 },
-		mapId: "DEMO_MAP_ID",
-	});
+	name.innerHTML = "Name"
+	description.innerHTML = "Description"
+	type.innerHTML = "Web Comment"
+	status.innerHTML = "Status"
+	date.innerHTML = "Date"
 
 	json[0].data.forEach((resource) => {
-		if (resource.latitude && resource.longitude && resource.acres) {
-			const marker = new AdvancedMarkerElement({
-				map: map,
-				position: { lat: parseFloat(resource.latitude), lng: -parseFloat(resource.longitude) },
-				title: resource.name,
-			});
-			console.log(`Name: ${resource.name} Latitude: ${resource.latitude} Longitude: ${resource.longitude}`)
-		}
+
+		let row = table_body.insertRow(-1)
+		let name = row.insertCell(0)
+		let description = row.insertCell(1)
+		let type = row.insertCell(2)
+		let status = row.insertCell(3)
+		let date = row.insertCell(4)
+
+		name.innerHTML = resource.name
+		description.innerHTML = resource.type
+		type.innerHTML = resource.webComment
+		status.innerHTML = resource.fire_status
+		date.innerHTML = resource.date
 	})
+
+	let main = document.getElementById("main")
+	table.appendChild(table_body)
+	main.appendChild(table)
+	return json
 }
 
-window.addEventListener('DOMContentLoaded', () => {
+let display_map = (incidents, perimeters) => {
+	let map = L.map('map').setView([44.1, -121.25], 10)
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> | <a href="https://data-nifc.opendata.arcgis.com/" target="_blank">NIFC</a>'
+	}).addTo(map)
+
+	L.geoJSON(perimeters).addTo(map)
+
+	let smallMarkerOptions = {
+		radius: 6,
+		fillColor: "#ff7800",
+		color: "#000",
+		weight: 1,
+		opacity: 1,
+		fillOpacity: 0.8
+	};
+
+	function onEachFeature(feature, layer) {
+		if (feature.properties && feature.properties.IncidentName) {
+			layer.bindPopup(`<b>Name: </b>${feature.properties.IncidentName}</br><b>Incident Size: </b>${feature.properties.IncidentSize} acres</br><b>Reported Date: </b>${new Date(feature.properties.CreatedOnDateTime_dt).toString()}</br><b>Last Updated: </b>${new Date(feature.properties.ModifiedOnDateTime_dt).toString()}`)
+		}
+	}
+
+	L.geoJSON(incidents, {
+		style: function(feature) {
+			if (feature.properties.IncidentSize > 1000) {
+				return {
+					fillColor: "#ff7800",
+					color: "#000",
+					weight: 2,
+					opacity: 1,
+					fillOpacity: 0.8
+				}
+			}
+		},
+		pointToLayer: function(feature, latlng) {
+			return L.circleMarker(latlng, smallMarkerOptions)
+		},
+		onEachFeature: onEachFeature
+	}).addTo(map);
+}
+
+window.addEventListener('DOMContentLoaded', async () => {
+	let response = await fetch("http://localhost:5000/api/incidents", { method: "GET", mode: "cors" })
+	if (!response.ok) {
+		throw new Error(`HTTP error! status: ${response.status}`);
+	}
+	const incidents = await response.json();
+	response = await fetch("http://localhost:5000/api/perimeters", { method: "GET", mode: "cors" })
+	if (!response.ok) {
+		throw new Error(`HTTP error! status: ${response.status}`);
+	}
+	const perimeters = await response.json();
 	display_headers()
-	display_resources_table("https://snknmqmon6.execute-api.us-west-2.amazonaws.com/centers/ORCOC/incidents")
+	display_map(incidents, perimeters)
 });
--- server.js
+++ server.js
@@ -1,7 +1,40 @@
 const express = require('express')
+const fs = require("node:fs")
+
 const app = express()
 const port = 5000
 
+let nifc_incidents = {}
+let wildcad_incidents = {}
+let nifc_perimeters = {}
+
 app.use(express.static('public'))
 
-app.listen(port, () => console.log(`Server listening at http://localhost:${port}`))
+app.listen(port, async () => { await get_incidents(); await get_perimeters(); console.log(`Server listening at http://localhost:${port}`) })
+
+app.get('/api/incidents', async (_, res) => {
+	res.status(200).header("Access-Control-Allow-Origin", "*").send(nifc_incidents)
+})
+
+app.get('/api/perimeters', async (_, res) => {
+	res.status(200).header("Access-Control-Allow-Origin", "*").send(nifc_perimeters)
+})
+
+let get_incidents = async () => {
+	console.log("Fetching new incidents...")
+	// WildWeb/WILDCADE API
+	//let response = await fetch("https://snknmqmon6.execute-api.us-west-2.amazonaws.com/centers/ORCOC/incidents") 
+	let response = await fetch("https://services3.arcgis.com/T4QMspbfLg3qTGWY/arcgis/rest/services/WFIGS_Incident_Locations_Current/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson")
+	nifc_incidents = await response.json()
+	// set a timer for 5 minutes
+	setTimeout(get_incidents, 10000 * 60)
+}
+
+let get_perimeters = async () => {
+	console.log("Fetching new perimeters...")
+	// NIFC ArcGIS API
+	let response = await fetch("https://services3.arcgis.com/T4QMspbfLg3qTGWY/arcgis/rest/services/WFIGS_Interagency_Perimeters_Current/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson")
+	nifc_perimeters = await response.json()
+	// set a timer for 5 minutes
+	setTimeout(get_perimeters, 30000 * 60)
+}
