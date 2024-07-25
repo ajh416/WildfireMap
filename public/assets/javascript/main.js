b69c619ca0019b69ba5cd5dc7cf257c555a7ac52
Adam Henry <henryada@oregonstate.edu>
2024-07-24 22:46:08 -0700

    remove pre-lazy load code

--- public/assets/javascript/main.js
+++ public/assets/javascript/main.js
@@ -4,6 +4,7 @@ let mobile_markers = {
 	10000: 16,
 	100000: 17,
 }
+
 let desktop_markers = {
 	1000: 6,
 	5000: 8,
@@ -17,137 +18,6 @@ let display_headers = () => {
 	subheader.textContent = "Incident Map (WIP)"
 	subheader.setAttribute('style', 'text-align: center;')
 	main.appendChild(subheader)
-}
-
-let display_map = (incidents, perimeters, firms) => {
-	let sat_layer = L.tileLayer("https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}", {
-		maxZoom: 19,
-		attribution: '&copy; <a href="https://www.google.com/permissions/geoguidelines.html" target="_blank">Google Maps</a> | <a href="https://data-nifc.opendata.arcgis.com/" target="_blank">NIFC</a>'
-	})
-	let osm_layer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		maxZoom: 19,
-		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> | <a href="https://data-nifc.opendata.arcgis.com/" target="_blank">NIFC</a>'
-	})
-
-	let map = L.map('map', {
-		center: [44.1, -121.25],
-		zoom: 8,
-		layers: [sat_layer, osm_layer]
-	})
-
-	let perim = L.geoJSON(perimeters, {
-		style: function(_) {
-			return { color: "#7777ff" }
-		}
-	}).addTo(map)
-
-	let firms_style = {
-		radius: 6,
-		fillColor: "#0000ff",
-		color: "#000",
-		weight: 1,
-		opacity: 1,
-		fillOpacity: 0.8
-	}
-	let firms_layer = L.geoJSON(firms, {
-		pointToLayer: function(feature, latlng) {
-			let style = firms_style
-			if (feature.properties.satellite === "N21")
-				style.color = "#00ff00"
-			if (feature.properties.satellite === "N20")
-				style.color = "#aaff00"
-			return L.circleMarker(latlng, style)
-		},
-		onEachFeature: function(feature, layer) {
-			if (feature.properties && feature.properties.acq_date) {
-				let brightness = feature.properties.bright_ti4
-				if (brightness === undefined)
-					brightness = "N/A"
-				layer.bindPopup(`<div id="popup-info"><b>Time: </b>${feature.properties.acq_date} ${feature.properties.acq_time} UTC</br><b>Brightness: </b>${brightness}</br><b>Source: </b>${feature.properties.satellite}`)
-			}
-		}
-	})
-
-	let baseMaps = {
-		"Google Maps Satellite": sat_layer,
-		"OpenStreetMap": osm_layer
-	}
-
-	/* Legend specific, from https://codepen.io/haakseth/pen/KQbjdO */
-	let legend = L.control({ position: "bottomleft" })
-
-	legend.onAdd = function(_) {
-		let div = L.DomUtil.create("div", "legend")
-		div.innerHTML += "<h4>Legend</h4>"
-		div.innerHTML += '<i style="background: #ff7800"></i><span>Incidents >24hrs old</span><br>'
-		div.innerHTML += '<i style="background: #ff5555"></i><span>Incidents <24hrs old</span><br>'
-		div.innerHTML += '<i style="background: #ff0000"></i><span>Incidents <12hrs old</span><br>'
-		div.innerHTML += '<i style="background: #7777ff"></i><span>Perimeters</span><br>'
-		div.innerHTML += '<i style="background: #0000ff"></i><span>NOAA/NASA FIRMS Heat Sources</span><br>'
-		return div
-	}
-
-	legend.addTo(map)
-
-	let smallMarkerOptions = {
-		radius: 6,
-		fillColor: "#ff7800",
-		color: "#000",
-		weight: 1,
-		opacity: 1,
-		fillOpacity: 0.8
-	}
-
-	function onEachFeature(feature, layer) {
-		let containment = feature.properties.PercentContained
-		if (containment === null || containment === undefined)
-			containment = '0'
-		if (feature.properties && feature.properties.IncidentName) {
-			layer.bindPopup(`<div id="popup-info"><b>Name: </b>${feature.properties.IncidentName}</br><b>Incident Size: </b>${feature.properties.IncidentSize} acres</br><b>Reported Date: </b>${new Date(feature.properties.CreatedOnDateTime_dt).toString()}</br><b>Last Updated: </b>${new Date(feature.properties.ModifiedOnDateTime_dt).toString()}</br><b>% Contained: </b>${containment}</div>`)
-		}
-	}
-
-	let inc = L.geoJSON(incidents, {
-		style: function(feature) {
-			let vars = {}
-			let size = parseInt(feature.properties.IncidentSize)
-			let it = {}
-			if (window.innerWidth < 1000)
-				it = mobile_markers
-			else
-				it = desktop_markers
-
-			if (size < 1000 || feature.properties.IncidentSize === null)
-				vars.radius = it[1000]
-			else if (size < 5000)
-				vars.radius = it[5000]
-			else if (size < 10000)
-				vars.radius = it[10000]
-			else
-				vars.radius = it[100000]
-
-			let d = new Date(feature.properties.CreatedOnDateTime_dt)
-			let now = new Date()
-			if (now - d < 3600 * 12 * 1000)
-				vars.fillColor = "#ff0000"
-			else if (now - d < 3600 * 24 * 1000)
-				vars.fillColor = "#ff5555"
-			return vars
-		},
-		pointToLayer: function(_, latlng) {
-			return L.circleMarker(latlng, smallMarkerOptions)
-		},
-		onEachFeature: onEachFeature
-	}).addTo(map);
-	firms_layer.addTo(map)
-	let overlays = {
-		"NASA/NOAA FIRMS": firms_layer,
-		"Perimeters": perim,
-		"Active Incidents": inc,
-	}
-	let control = L.control.layers(baseMaps, overlays).addTo(map)
-	// disable FIRMS layer by default
-	control._map.removeLayer(firms_layer)
 }
 
 window.addEventListener('DOMContentLoaded', async () => {
@@ -172,7 +42,8 @@ window.addEventListener('DOMContentLoaded', async () => {
 		throw new Error(`HTTP error! status: ${response.status}`)
 	}
 	const incidents = await response.json()
-	let inc = display_incidents(map, incidents)
+	let inc = display_incidents(incidents)
+	inc.addTo(map)
 
 	div.innerHTML = "<h1>Loading perimeters...</h1>"
 	response = await fetch("https://wildfire-map.com/api/perimeters", { method: "GET", mode: "cors" })
@@ -180,7 +51,10 @@ window.addEventListener('DOMContentLoaded', async () => {
 		throw new Error(`HTTP error! status: ${response.status}`)
 	}
 	const perimeters = await response.json()
-	let perim = display_perimeters(map, perimeters)
+	let perim = display_perimeters(perimeters)
+	map.removeLayer(inc)
+	perim.addTo(map)
+	inc.addTo(map)
 
 	div.innerHTML = "<h1>Loading FIRMS data...</h1>"
 	response = await fetch("https://wildfire-map.com/api/firms", { method: "GET", mode: "cors" })
@@ -188,7 +62,7 @@ window.addEventListener('DOMContentLoaded', async () => {
 		throw new Error(`HTTP error! status: ${response.status}`)
 	}
 	const firms = await response.json()
-	let firms_layer = display_firms(map, firms)
+	let firms_layer = display_firms(firms)
 
 	let baseMaps = {
 		"Google Maps Satellite": sat_layer,
@@ -241,7 +115,7 @@ let init_map = () => {
 	return [map, sat_layer, osm_layer]
 }
 
-let display_incidents = (map, incidents) => {
+let display_incidents = (incidents) => {
 	let smallMarkerOptions = {
 		radius: 6,
 		fillColor: "#ff7800",
@@ -287,22 +161,22 @@ let display_incidents = (map, incidents) => {
 			return L.circleMarker(latlng, smallMarkerOptions)
 		},
 		onEachFeature: onEachFeature
-	}).addTo(map)
+	})
 
 	return inc
 }
 
-let display_perimeters = (map, perimeters) => {
+let display_perimeters = (perimeters) => {
 	let perim = L.geoJSON(perimeters, {
 		style: function(_) {
 			return { color: "#7777ff" }
 		}
-	}).addTo(map)
+	})
 
 	return perim
 }
 
-let display_firms = (map, firms) => {
+let display_firms = (firms) => {
 	let firms_style = {
 		radius: 6,
 		fillColor: "#0000ff",
@@ -312,12 +186,8 @@ let display_firms = (map, firms) => {
 		fillOpacity: 0.8
 	}
 	let firms_layer = L.geoJSON(firms, {
-		pointToLayer: function(feature, latlng) {
+		pointToLayer: function(_, latlng) {
 			let style = firms_style
-			if (feature.properties.satellite === "N21")
-				style.color = "#00ff00"
-			if (feature.properties.satellite === "N20")
-				style.color = "#aaff00"
 			return L.circleMarker(latlng, style)
 		},
 		onEachFeature: function(feature, layer) {
@@ -329,8 +199,6 @@ let display_firms = (map, firms) => {
 			}
 		}
 	})
-	firms_layer.addTo(map)
-	map.removeLayer(firms_layer)
 
 	return firms_layer
 }
